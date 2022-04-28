a1e13ffe22731acdda12da3e0bf5fe8b1637cbf8
Diego Fagundes <83848088+diegofagundesr@users.noreply.github.com>
2022-04-28 13:31:26 -0300

    Add files via upload

---
+++ #1001.py
@@ -0,0 +1,12 @@
+#beecrowd | 1001
+
+# 1001
+
+# Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. Imprima X conforme exemplo apresentado abaixo.
+# ler valores inteiros A e B
+A = int(input())
+B = int(input())
+X = (A + B)
+
+# Imprimir o resultado da soma
+print("X =", X)
---
+++ #1004.py
@@ -0,0 +1,13 @@
+#beecrowd | 1004
+
+# Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD
+
+# Ler dois valores inteiros
+valor1 = int(input())
+valor2 = int(input())
+
+# Cálculo do produto das variáveis
+PROD = valor1 * valor2
+
+# Imprimir o resultado do produto, PROD, atribuído à mensagem PROD
+print("PROD =", PROD)

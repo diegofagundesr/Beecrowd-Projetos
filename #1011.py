0a01864a2cd10df0a0cdc9d30954474da18f6184
Diego Fagundes <83848088+diegofagundesr@users.noreply.github.com>
2022-04-29 21:27:25 -0300

    Add files via upload

--- #1011.py
+++ #1011.py
@@ -1,38 +1,13 @@
-# beecrowd | 1012
+# beecrowd | 1011
 
-# Escreva um programa que leia três valores com ponto flutuante de dupla precisão: A, B e C. Em seguida, calcule e mostre:
-# a) a área do triângulo retângulo que tem A por base e C por altura.
-# b) a área do círculo de raio C. (pi = 3.14159)
-# c) a área do trapézio que tem A e B por bases e C por altura.
-# d) a área do quadrado que tem lado B.
-# e) a área do retângulo que tem lados A e B.
+# Faça um programa que calcule e mostre o volume de uma esfera sendo fornecido o valor de seu raio (R). A fórmula para calcular o volume é: (4/3) * pi * R3
 
-# receber três valores A,B e C
-valores = input().split()
-A = float(valores[0])
-B = float(valores[1])
-C = float(valores[2])
+# ler o raio fornecido
+raio = float(input())
 
-# calcular área das figuras
-# area do triangulo
-area_triangulo = (A * C) / 2
-
-# area do círculo
+# fórmula e pi
 pi = 3.14159
-area_circulo = (pi * C**2)
+volume = ((4/3) * pi * raio**3)
 
-# area do trapezio
-area_trapezio = ((A + B) * C) / 2
-
-# area do quadrado
-area_quadrado = (B * B)
-
-# area do retângulos
-area_retangulo = (A * B)
-
-# mostrar resultado dos calculos das areas da figuras correspondentes
-print("TRIANGULO: "'{:.3f}'.format(area_triangulo))
-print("CIRCULO: "'{:.3f}'.format(area_circulo))
-print("TRAPEZIO: "'{:.3f}'.format(area_trapezio))
-print("QUADRADO: "'{:.3f}'.format(area_quadrado))
-print("RETANGULO: "'{:.3f}'.format(area_retangulo))
+# imprimir resultado do calculo com a mensagem VOLUME
+print("VOLUME =", '{:.3f}'.format(volume))
